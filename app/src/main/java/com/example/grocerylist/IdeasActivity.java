aee7dae43c63065391bdf11ba30803b0605f04d5
marija-21 <58482257+marija-21@users.noreply.github.com>
2021-02-10 20:41:09 +0100

    dodaeniproducts

--- app/src/main/java/com/example/grocerylist/IdeasActivity.java
+++ app/src/main/java/com/example/grocerylist/IdeasActivity.java
@@ -27,7 +27,13 @@ public class IdeasActivity extends AppCompatActivity {
         setContentView(R.layout.activity_ideas);
 
 
-        List<String> values = Arrays.asList("Fruits&Veggies", "DairyProducts", "MeatProducts", "Pasta","HygieneProducts", "CleaningProducts", "Alcohol" );
+        List<String> values = Arrays.asList("•Tomatoes\n•Cucumber\n•Cabbage\n•Kiwi\n•Banana\n•Blueberry\n•Lemon\n•Chilli Pepper\n•Cherry\n•Eggplant",
+                "•Butter\n•Cheese\n•Yoghurt\n•Milk\n•Sour Cream\n•Kefir\n•Feta\n•Cottage Cheese\n•Condensed Milk",
+                "•Chicken Steak\n•Sausage\n•Pork\n•Bacon\n•Meatballs\n•Chicken Breasts\n•Ribs\n•Tuna\n•Salami",
+                "•Spaghetti\n•Lasagna\n•Fusilli\n•Gnocchi\n•Riciolli\n•Ravioli\n•Orzo\n•Penne\n•Ditalini",
+                "•Shampoo\n•Conditioner\n•Toothpaste\n•Breath strips\n•Dental floss\n•Mouth wash\n•Hand Soap\n•Cotton Buds\n•Razor",
+                "•Sponge\n•Detergent\n•Thick Bleach\n•Laundry Soap\n•Gloves\n•Shower Cleaner\n•Soft Scrub\n•Wet Wipes\n•Domestos",
+                "•Wine\n•Vodka\n•Beer\n•Whiskey\n•Gin\n•Rum\n•Sangria\n•Tequila\n•Brandy\n•Negroni" );
         mRecyclerView = (RecyclerView) findViewById(R.id.list);
         mRecyclerView.setHasFixedSize(true);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
